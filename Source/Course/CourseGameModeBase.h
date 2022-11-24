34d1e826238e72e16e747dc7e7517649b8858c8e
付有芳 <fuyoufang@163.com>
2022-11-24 16:01:44 +0800

    init

---
+++ Source/Course/CourseGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CourseGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class COURSE_API ACourseGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
